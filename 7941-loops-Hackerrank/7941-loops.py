4f1e66de5d505f68096439364a09b10c0da6d601
shashankkateel7-creator <shashankkateel7@gmail.com>
2026-08-25 21:53:22 +0530

    #5

---
+++ 7941-loops-Hackerrank/7941-loops.py
@@ -0,0 +1,9 @@
+if __name__ == '__main__':
+    n = int(input())
+    for i in range(n):
+        print(i*i)
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
