e47a6fc70bbd1b3a3e6f029898392322c98adf56
shashankkateel7-creator <shashankkateel7@gmail.com>
2026-08-26 17:07:22 +0530

    #7

---
+++ 8026-print-function-Hackerrank/8026-print-function.py
@@ -0,0 +1,9 @@
+if __name__ == '__main__':
+    n = int(input())
+    for i in range(n):
+        print(i+1,sep="",end="")
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
